21a998daee0a2d37e58613cc08612dbda1c64ea0
Sourya Kakarla <sk5057@columbia.edu>
2022-03-21 07:02:23 +0000

    Remove cropping for NH test

--- data_utils/NH.py
+++ data_utils/NH.py
@@ -27,10 +27,11 @@ class RESIDE_Dataset(data.Dataset):
         print(f'path={path}')
         super(RESIDE_Dataset,self).__init__()
         self.size = size
-        print('crop size',size)
+        print('------------------- crop size',size)
         self.train=train
 
         self.h5path = path # 输入h5文件
+        print(f"----len {self.__len__()}")
     def __getitem__(self, index):
 
         file_name = self.h5path + '/' + str(index) + '.h5'
@@ -38,21 +39,33 @@ class RESIDE_Dataset(data.Dataset):
         haze = f['haze']
         clear = f['gt']
 
+        orig_name = haze.attrs['file_name']
+
         haze = Image.fromarray(np.array(haze))
         clear = Image.fromarray(np.array(clear))
         #print(f'haze={haze}')
         #exit()
+        """
         clear = tfs.CenterCrop(haze.size[::-1])(clear)
 
         if not isinstance(self.size, str):
             i, j, h, w = tfs.RandomCrop.get_params(haze,output_size=(self.size,self.size))
             haze = FF.crop(haze, i, j, h, w)
             clear = FF.crop(clear, i, j, h, w)
+        """
 
-        haze, clear = self.augData(haze.convert("RGB"), clear.convert("RGB") )
+        clear = tfs.CenterCrop(haze.size[::-1])(clear)
+        haze2, clear2 = self.augData(haze.convert("RGB"), clear.convert("RGB") )
+
+        #haze2 = tfs.ToTensor()(haze.convert("RGB"))
+        #clear2 = tfs.ToTensor()(clear.convert("RGB"))
         #print(f'haze={haze.shape}')
         #exit()
-        return haze, clear
+        print("------------ GET ITEM-------")
+        print(haze2.size())
+        print(clear2.size())
+        #return haze2, clear2
+        return haze2, clear2, orig_name
 
     def augData(self, data, target):
 
@@ -79,7 +92,7 @@ root = opt.root
 
 print(f"------------------        ROOT {root} -----------------------")
 
-NH_train_loader=DataLoader(dataset=RESIDE_Dataset(root+'NH_train/', train=True,size=crop_size),batch_size=BS,shuffle=True)
+#NH_train_loader=DataLoader(dataset=RESIDE_Dataset(root+'NH_train/', train=True,size=crop_size),batch_size=BS,shuffle=True)
 NH_test_loader=DataLoader(dataset=RESIDE_Dataset(root+'NH_test/',train=False,size='whole img'),batch_size=1,shuffle=False)
 # for debug
 #ITS_test = '/home/why/workspace/CDNet/net/debug/test_h5/'
