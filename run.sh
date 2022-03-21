d5fe7b4ea682752c86b9dca73d8be1724df472fe
Sourya Kakarla <sk5057@columbia.edu>
2022-03-21 07:00:02 +0000

    Add root to run

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 export PYTHONPATH=$PYTHONPATH:./models/
 
 
-python3 train_aecrnet.py --testset "NH_test" --resume True --pre_model "NH_train.pk" --eval_dataset True
+python3 train_aecrnet.py --testset "NH_test" --resume True --pre_model "NH_train.pk" --eval_dataset True --model_name "NH_train" --root "/home/sk5057/AECR-Net/"
