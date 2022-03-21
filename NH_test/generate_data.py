12cff3708563032e0f38784bc19e1c5946273c72
Sourya Kakarla <sourya4@ladduu.com>
2022-03-20 21:25:29 -0400

    Test original name attribute in h5 file

--- NH_test/generate_data.py
+++ NH_test/generate_data.py
@@ -12,7 +12,7 @@ def generate_h5():
 
     for i, image_name in enumerate(image_list):
         img_np = np.array(Image.open(image_name))
-        ground_img_np = np.array(Image.open("./ground/"+image_name))
+        ground_img_np = np.array(Image.open("./ground/"+"075.jpg"))
         hf = h5py.File(f"{i}.h5", 'a') # open a hdf5 file
         print(i, image_name)
         dset = hf.create_dataset('haze', data=img_np)  # write the data to hdf5 file
@@ -27,13 +27,15 @@ def open_h5():
     for i,image_name in enumerate(image_list):
         f = h5py.File(image_name, 'r')
         haze = f['haze']
+        print(haze.attrs['file_name'])
         clear = f['gt']
         haze = Image.fromarray(np.array(haze))
         clear = Image.fromarray(np.array(clear))
 
-        haze.show()
-        clear.show()
+
+        #haze.show()
+        #clear.show()
 
 if __name__ == "__main__":
-    generate_h5()
-    # open_h5()
+    #generate_h5()
+    open_h5()
