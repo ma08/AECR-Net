3426b36e0ab229e23195b67565a6b46cda7e494b
Sourya Kakarla <sk5057@columbia.edu>
2022-03-21 06:53:58 +0000

    Modify h5 generation

--- NH_test/generate_data.py
+++ NH_test/generate_data.py
@@ -12,7 +12,7 @@ def generate_h5():
 
     for i, image_name in enumerate(image_list):
         img_np = np.array(Image.open(image_name))
-        ground_img_np = np.array(Image.open("./ground/"+"075.jpg"))
+        ground_img_np = np.array(Image.open(f"./ground/{image_name}"))
         hf = h5py.File(f"{i}.h5", 'a') # open a hdf5 file
         print(i, image_name)
         dset = hf.create_dataset('haze', data=img_np)  # write the data to hdf5 file
@@ -37,5 +37,5 @@ def open_h5():
         #clear.show()
 
 if __name__ == "__main__":
-    #generate_h5()
-    open_h5()
+    generate_h5()
+    #open_h5()
