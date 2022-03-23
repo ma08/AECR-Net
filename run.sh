6c604b1570e3018ab0f6723e5e4757b404f04d34
Sourya Kakarla <sk5057@columbia.edu>
2022-03-23 07:27:51 +0000

    Submission day commit (March 21 2022)

--- run.sh
+++ run.sh
@@ -5,4 +5,5 @@ export PYTHONPATH=$PYTHONPATH:./models/
 #python3 train_aecrnet.py --testset "NH_test" --resume True --pre_model "NH_train.pk" --eval_dataset True --model_name "NH_train" --root "/home/sk5057/AECR-Net/"
 
 
-python3 train_aecrnet.py --resume True --trainset "NH_train" --testset "NH_test" --pre_model "NH_train.pk" --model_name "NH_train" --root "/home/sk5057/AECR-Net/" --eval_step 30 --bs 2
+python3 train_aecrnet.py --resume True --trainset "NH_train" --testset "NH_test" --pre_model "NH_train.pk" --model_name "NH_train" --root "/home/sk5057/AECR-Net/" --eval_step 30 --bs 1 --lr 0.0001
+
